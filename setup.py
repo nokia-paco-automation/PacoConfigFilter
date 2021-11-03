3948d98dd5696cf2675b0c1bf36b5237fd7a062c
Markus Vahlenkamp <markus.vahlenkamp@nokia.com>
2021-11-03 21:54:04 +0100

    setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='PacoConfigFilter',
+    version='',
+    packages=[''],
+    url='',
+    license='',
+    author='MVahlenkamp',
+    author_email='markus@vahlenkamp.net',
+    description=''
+)
